068b09e3182818ab638a77bc9f0a9c3eb3c932c5
mcanela30 <60703913+mcanela30@users.noreply.github.com>
2020-10-29 17:21:35 +0100

    inici amb error.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IniciComponent } from './inici/inici.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IniciComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
